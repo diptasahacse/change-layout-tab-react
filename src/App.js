08b26ea458f09e781d0d52449a9ee22095c59a23
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-06-24 01:42:19 +0600

    company assignment task with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [selectedLayout, setSelectedLayout] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mx-auto' style={{maxWidth:"1200px", width:"100%"}}>
+      <div>
+        <button className={` normal-button-bg  py-2 px-3 rounded-lg m-1 ${(selectedLayout == '' || selectedLayout == 'layout1') && 'active-button'}`} onClick={() =>setSelectedLayout('layout1')}>Layout 1</button>
+        <button className={` normal-button-bg  py-2 px-3 rounded-lg m-1 ${(selectedLayout == 'layout2') && 'active-button'}`}  onClick={() =>setSelectedLayout('layout2')}>Layout 2</button>
+        <button className={` normal-button-bg  py-2 px-3 rounded-lg m-1 ${(selectedLayout == 'layout3') && 'active-button'}`}  onClick={() =>setSelectedLayout('layout3')}>Layout 3</button>
+      </div>
+
+
+
+      <div className={`grid gap-2 ${selectedLayout == 'layout2' && 'grid-cols-3'} ${selectedLayout == 'layout3' && 'grid-cols-4'}`}>
+        <div className={`card-bg-color flex justify-center items-center rounded-lg py-4 text-white ${selectedLayout == 'layout3' && 'col-span-4'} ${selectedLayout == 'layout2' && 'h-80'}`}>Card 1</div>
+        <div className={`card-bg-color flex justify-center items-center rounded-lg py-4 text-white ${selectedLayout == 'layout2' && 'h-80'}  ${selectedLayout == 'layout3' && 'h-96'}`}>Card 2</div>
+        <div className={`card-bg-color flex justify-center items-center rounded-lg py-4 text-white ${selectedLayout == 'layout3' && 'col-span-2 h-96'} ${selectedLayout == 'layout2' && 'h-80'}`}>Card 3</div>
+        <div className={`card-bg-color flex justify-center items-center rounded-lg py-4 text-white  ${selectedLayout == 'layout2' && 'h-80'}  ${selectedLayout == 'layout3' && 'h-96'}`}>Card 4</div>
+        <div className={`card-bg-color flex justify-center items-center rounded-lg py-4 text-white ${selectedLayout == 'layout3' && 'col-span-4'} ${selectedLayout == 'layout2' && 'h-80'}`}>Card 5</div>
+      </div>
+
+
     </div>
   );
 }
